3ebd61cb41e673c3cd75ed6a392b7199352ef284
Mimi Reyburn <mimireyburn@gmail.com>
2024-04-18 18:58:12 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip3 install bitsandbytes
+pip3 install -q -U peft==0.8.2
+pip3 install -q -U trl==0.7.10
+pip3 install accelerate
+pip3 install -q -U datasets==2.17.0
+pip3 install -q -U transformers==4.38.1
+pip3 install python-dotenv
